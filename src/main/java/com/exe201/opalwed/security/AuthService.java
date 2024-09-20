b4239abf513acbf550fc9961a776c777140c2b43
Hoang-Phuc <phucnhse171734@fpt.edu.vn>
2024-09-20 16:28:03 +0700

    phuc/fix swagger header

--- src/main/java/com/exe201/opalwed/security/AuthService.java
+++ src/main/java/com/exe201/opalwed/security/AuthService.java
@@ -6,12 +6,22 @@ import com.exe201.opalwed.model.Information;
 import com.exe201.opalwed.model.Role;
 import com.exe201.opalwed.repository.AccountRepository;
 import com.exe201.opalwed.repository.InformationRepository;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -69,6 +79,62 @@ public class AuthService {
         result.put("token", token);
 
         return result;
+    }
+
+    public Map<String, Object> loginWithGoogle(String requestToken) {
+
+        try {
+            URL url = new URL("https://www.googleapis.com/oauth2/v3/userinfo");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization", "Bearer " + requestToken);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                JsonParser jsonParser = JsonParserFactory.getJsonParser();
+                Map<String, Object> jsonData = jsonParser.parseMap(response.toString());
+                String email = (String) jsonData.get("email");
+                String givenName = (String) jsonData.get("given_name");
+                String picture = (String) jsonData.get("picture");
+                Optional<Account> userAccountCheck = accountRepository.findAccountByEmail(email);
+                Account userAccount;
+                Information newInformation = null;
+                if(userAccountCheck.isPresent()){
+                    userAccount = userAccountCheck.get();
+                }else{
+                    userAccount = Account.builder()
+                            .email(email)
+                            .password(passwordEncoder.encode(UUID.randomUUID().toString()))
+                            .accountRole(Role.CUSTOMER)
+                            .build();
+                    accountRepository.save(userAccount);
+                    newInformation = Information.builder()
+                            .account(userAccount)
+                            .fullName(givenName)
+                            .imageUrl(picture)
+                            .build();
+                    informationRepository.save(newInformation);
+                }
+                String jwtToken = jwtProvider.createToken(newInformation.getAccount());
+
+                Map<String, Object> result = new HashMap<>();
+                userAccount.setPassword("");
+                result.put("userInfo", userAccount);
+                result.put("token", jwtToken);
+
+                return result;
+            } else {
+                throw new OpalException("Invalid Google token");
+            }
+        } catch (Exception e) {
+            throw new OpalException("Login google failed");
+        }
     }
 
 }
