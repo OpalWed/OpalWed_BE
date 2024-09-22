f1cef1fa1676bcd30f642c21e1ed7c139a2f52de
Hoang-Phuc <phucnhse171734@fpt.edu.vn>
2024-09-22 14:36:47 +0700

    phuc/Response object

--- src/main/java/com/exe201/opalwed/controller/PartnerController.java
+++ src/main/java/com/exe201/opalwed/controller/PartnerController.java
@@ -1,9 +1,11 @@
 package com.exe201.opalwed.controller;
 
 import com.exe201.opalwed.dto.PartnerInformationDTO;
+import com.exe201.opalwed.dto.ResponseObject;
 import com.exe201.opalwed.service.PartnerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,19 @@ public class PartnerController {
 
 
     @PostMapping("/new")
-    public PartnerInformationDTO connectNewPartner(@Valid @RequestBody PartnerInformationDTO partnerInformation) {
-        return partnerService.connectNewPartner(partnerInformation);
+    public ResponseEntity<ResponseObject> connectNewPartner(@Valid @RequestBody PartnerInformationDTO partnerInformation) {
+        PartnerInformationDTO data = partnerService.connectNewPartner(partnerInformation);
+
+        var responseObject = ResponseObject.builder()
+                .message("Add new partner successfully!")
+                .isSuccess(true)
+                .data(data)
+                .build();
+        return ResponseEntity.ok().body(responseObject);
+
+
+
+
     }
 
 
